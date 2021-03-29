60756058b5acd54e075541a2c29fc950f4352e66
rcollmenendezUOC <81294382+rcollmenendezUOC@users.noreply.github.com>
2021-03-29 12:56:01 +0200

    v1.2 - Primera versión para revisión

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # UOC_WebScraping_v1
 
-jodete lorito
+Pasos realizados para el escaneo de la página WPT:
+
+1. Abrir la URL principal del ranking y hacer scroll down
+
+2. Extraer del código HTML de la página completa las URLs de la ficha de cada jugador
+
+3. Acceder a la ficha de cada jugador y extraer la información deseada
+
+4. Crear la estructura de datos y el fichero CSV con la información anterior
+
+
--- WPT_PlayersRanking.py
+++ WPT_PlayersRanking.py
@@ -1,55 +1,70 @@
 """
-@author:
-
+@author: Ruben Coll Menendez
 """
 
 """
     Configurando el entorno
 """
+
 import time
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import pandas as pd
 
 """
     Inicio
 """
 
-# Web scrapper for infinite scrolling page
+# Cargar la página con scroll infinito
 driver = webdriver.Chrome(executable_path='C:/Users/RCOLL/Downloads/chromedriver_win32/chromedriver.exe')
 driver.delete_all_cookies()
 driver.get("https://www.worldpadeltour.com/jugadores?ranking=masculino")
-time.sleep(2)  # Allow 2 seconds for the web page to open
-scroll_pause_time = 2 # You can set your own pause time. My laptop is a bit slow so I use 1 sec
-screen_height = driver.execute_script("return window.screen.height;")   # get the screen height of the web
+time.sleep(2)
+scroll_pause_time = 2
+# Tamaño inicial de la página
+screen_height = driver.execute_script("return window.screen.height;")
 i = 1
 
 while True:
-    # scroll one screen height each time
     driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))
     i += 1
     time.sleep(scroll_pause_time)
-    # update scroll height each time after scrolled, as the scroll height can change after we scrolled the page
+    # Capturamos la altura de la página
     scroll_height = driver.execute_script("return document.body.scrollHeight;")
     # Break the loop when the height we need to scroll to is larger than the total scroll height
     if (screen_height) * i > scroll_height:
         break
 
-##### Extract Reddit URLs #####
-urls = []
-soup = BeautifulSoup(driver.page_source, "html.parser")
+# Extraer URLS de los jugadores
 
+'''
+Una vez tenemos la página con todo el ranking de jugadores cargada,
+procedemos a la extracción de las URLs de cada uno de ellos para, posteriormente,
+cargar cada una de ellas y extraer la información individual de cada jugador
+'''
+
+soup = BeautifulSoup(driver.page_source, "html.parser")
+driver.quit()
+
+urls = []
 for element in soup.find_all("li", {"class": "c-player-card__item"}):
     name = element.find("a", {"class": "c-trigger"})['href']
     urls.append(name)
 
-for x in urls:
-   print(x)
+
+# Comprobamos que se han cargado las URLs en la lista
+print(len(urls))
+
+# Limpiamos el entorno
+del soup
 
 """
     Información Jugador
 """
 
 # Abrir URL de cada jugador
+
+driver = webdriver.Chrome(executable_path='C:/Users/RCOLL/Downloads/chromedriver_win32/chromedriver.exe')
 driver.delete_all_cookies()
 driver.get(urls[0])
 content = driver.page_source
@@ -57,6 +72,7 @@ soup = BeautifulSoup(content,'html.parser')
 driver.quit() # Force browser to quit when we have the data
 
 # Extracción info
+nombre = soup.find_all('h1',{'class': 'c-ranking-header__title'})[0].get_text()
 ranking_actual = soup.find_all('p',{'class': 'c-ranking-header__data'})[0].get_text()
 puntos = soup.find_all('p',{'class': 'c-ranking-header__data'})[1].get_text()
 p_jugados = soup.find_all('p',{'class': 'c-ranking-header__data'})[2].get_text()
@@ -66,22 +82,25 @@ efectividad = soup.find_all('p',{'class': 'c-ranking-header__data'})[5].get_text
 vict_consecutivas = soup.find_all('p',{'class': 'c-ranking-header__data'})[6].get_text()
 url = urls[0] # Cambiar el 0 por el índice al hacer el loop
 
-player_dict_2 = {}
+player_dict = {}
 
-player_dict_2['ranking_actual'] = ranking
-player_dict_2['puntos'] = puntos
-player_dict_2['p_jugados'] = p_jugados
-player_dict_2['p_ganados'] = p_ganados
-player_dict_2['p_perdidos'] = p_perdidos
-player_dict_2['efectividad'] = efectividad
-player_dict_2['vict_consecutivas'] = vict_consecutivas
-player_dict_2['url'] = url
+player_dict['nombre'] = nombre
+player_dict['ranking_actual'] = ranking_actual
+player_dict['puntos'] = puntos
+player_dict['p_jugados'] = p_jugados
+player_dict['p_ganados'] = p_ganados
+player_dict['p_perdidos'] = p_perdidos
+player_dict['efectividad'] = efectividad
+player_dict['vict_consecutivas'] = vict_consecutivas
+player_dict['url'] = url
 
-nombre = soup.find_all('h1',{'class': 'c-ranking-header__title'})[0].get_text()
+print(player_dict)
 
-player_dict_1 = {}
+# Diccionario a pandas df
 
-player_dict_1['nombre'] = name
-player_dict_1['details'] = player_dict_2
+df = pd.DataFrame(player_dict, index=[0])
+df.head()
 
-print(player_dict_1)
+# Pandas df a CSV
+
+
