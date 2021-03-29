c2853231e57f118ac9221359d28bf3177ae0663e
rcollmenendezUOC <81294382+rcollmenendezUOC@users.noreply.github.com>
2021-03-29 11:54:40 +0200

    v1.1 - Script consolidado

--- WPT_PlayersRanking.py
+++ WPT_PlayersRanking.py
@@ -16,7 +16,8 @@ from bs4 import BeautifulSoup
 
 # Web scrapper for infinite scrolling page
 driver = webdriver.Chrome(executable_path='C:/Users/RCOLL/Downloads/chromedriver_win32/chromedriver.exe')
-driver.get("https://www.worldpadeltour.com/jugadores?ranking=todos")
+driver.delete_all_cookies()
+driver.get("https://www.worldpadeltour.com/jugadores?ranking=masculino")
 time.sleep(2)  # Allow 2 seconds for the web page to open
 scroll_pause_time = 2 # You can set your own pause time. My laptop is a bit slow so I use 1 sec
 screen_height = driver.execute_script("return window.screen.height;")   # get the screen height of the web
@@ -33,7 +34,6 @@ while True:
     if (screen_height) * i > scroll_height:
         break
 
-
 ##### Extract Reddit URLs #####
 urls = []
 soup = BeautifulSoup(driver.page_source, "html.parser")
@@ -46,21 +46,42 @@ for x in urls:
    print(x)
 
 """
-Información Jugador
+    Información Jugador
 """
 
-urls[0]
-
-
-driver = webdriver.Chrome(executable_path='C:/Users/RCOLL/Downloads/chromedriver_win32/chromedriver.exe')
-# Abrimos la página con Chrome
+# Abrir URL de cada jugador
 driver.delete_all_cookies()
-driver.get('https://www.worldpadeltour.com/jugadores/alejandro-galan-romo')
+driver.get(urls[0])
 content = driver.page_source
 soup = BeautifulSoup(content,'html.parser')
 driver.quit() # Force browser to quit when we have the data
 
+# Extracción info
+ranking_actual = soup.find_all('p',{'class': 'c-ranking-header__data'})[0].get_text()
+puntos = soup.find_all('p',{'class': 'c-ranking-header__data'})[1].get_text()
+p_jugados = soup.find_all('p',{'class': 'c-ranking-header__data'})[2].get_text()
+p_ganados = soup.find_all('p',{'class': 'c-ranking-header__data'})[3].get_text()
+p_perdidos = soup.find_all('p',{'class': 'c-ranking-header__data'})[4].get_text()
+efectividad = soup.find_all('p',{'class': 'c-ranking-header__data'})[5].get_text()
+vict_consecutivas = soup.find_all('p',{'class': 'c-ranking-header__data'})[6].get_text()
+url = urls[0] # Cambiar el 0 por el índice al hacer el loop
 
-ranking = soup.find_all('p',{'class': 'c-ranking-header__data'})[0]
+player_dict_2 = {}
 
+player_dict_2['ranking_actual'] = ranking
+player_dict_2['puntos'] = puntos
+player_dict_2['p_jugados'] = p_jugados
+player_dict_2['p_ganados'] = p_ganados
+player_dict_2['p_perdidos'] = p_perdidos
+player_dict_2['efectividad'] = efectividad
+player_dict_2['vict_consecutivas'] = vict_consecutivas
+player_dict_2['url'] = url
 
+nombre = soup.find_all('h1',{'class': 'c-ranking-header__title'})[0].get_text()
+
+player_dict_1 = {}
+
+player_dict_1['nombre'] = name
+player_dict_1['details'] = player_dict_2
+
+print(player_dict_1)
