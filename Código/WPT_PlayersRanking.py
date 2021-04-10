7220860e50d512080a95f88ce793be7c41321604
rcollmenendezUOC <81294382+rcollmenendezUOC@users.noreply.github.com>
2021-04-10 14:37:19 +0200

    Update WPT_PlayersRanking.py

--- Código/WPT_PlayersRanking.py
+++ Código/WPT_PlayersRanking.py
@@ -33,6 +33,7 @@ screen_height = driver.execute_script('return window.screen.height;')
 i = 1
 while True:
     driver.execute_script('window.scrollTo(0, {screen_height}*{i});'.format(screen_height=screen_height, i=i))
+    
     i += 1
     time.sleep(scroll_pause_time)
     scroll_height = driver.execute_script('return document.body.scrollHeight;')
